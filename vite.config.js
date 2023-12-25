9ca5aa839b21e38c145daf4c09cacbe56d44d7de
kushnarev_nm <kushnarev_nm@nlmk.com>
2023-12-26 01:53:06 +0300

    fix(sprint_1): vite config fix

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import eslint from "vite-plugin-eslint";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   build: {
     outDir: "dist",
   },
