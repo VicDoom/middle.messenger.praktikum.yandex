0a362292b71efd4681d118b34499592e7432aad1
kushnarev_nm <kushnarev_nm@nlmk.com>
2023-12-26 13:06:23 +0300

    fix(sprint_1): icon fixes

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,30 @@ import { CHAT_ELEMENTS, CURRENT_CHAT } from "./mocks";
 import Handlebars from "handlebars";
 import { getCroppedText, getTime } from "./helpers";
 
+import iconAdd from "/public/icons/icon-add.svg";
+import iconAvatar from "/public/icons/icon-avatar.svg";
+import iconBack from "/public/icons/icon-back.svg";
+import iconClose from "/public/icons/icon-close.svg";
+import iconDelete from "/public/icons/icon-delete.svg";
+import iconExpand from "/public/icons/icon-expand.svg";
+import iconForward from "/public/icons/icon-forward.svg";
+import iconPin from "/public/icons/icon-pin.svg";
+import iconSearch from "/public/icons/icon-search.svg";
+import iconSend from "/public/icons/icon-send.svg";
+
+const Icons: { [name: string]: string } = {
+  "icon-avatar": iconAvatar,
+  "icon-add": iconAdd,
+  "icon-back": iconBack,
+  "icon-close": iconClose,
+  "icon-delete": iconDelete,
+  "icon-expand": iconExpand,
+  "icon-forward": iconForward,
+  "icon-pin": iconPin,
+  "icon-search": iconSearch,
+  "icon-send": iconSend,
+};
+
 Object.entries({ ...Layouts, ...Components, ...ChatComponents }).forEach(([name, component]) => {
   Handlebars.registerPartial(name, component);
 });
@@ -23,6 +47,10 @@ Handlebars.registerHelper("ifEquals", function(arg1: string, arg2: string, optio
 Handlebars.registerHelper("ifNotEqual", function(arg1: string, arg2: string, options: any) {
   //@ts-expect-error
   return (arg1 != arg2) ? options.fn(this) : options.inverse(this);
+});
+
+Handlebars.registerHelper("icons", function(arg: string) {
+  return Icons[arg];
 });
 
 interface IPageList {
@@ -61,7 +89,7 @@ function navigate(page: string) {
   container.innerHTML = Handlebars.compile(source)(context);
 }
 
-document.addEventListener("DOMContentLoaded", () => navigate("login"));
+document.addEventListener("DOMContentLoaded", () => navigate("profile"));
 
 document.addEventListener("click", (e) => {
   //@ts-expect-error
