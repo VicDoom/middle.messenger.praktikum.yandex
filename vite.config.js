105499235df8ed40bb26be43796d8005d07bcfae
Nikita Kushnaryov <kushnarev_nm@nlmk.com>
2024-01-05 19:05:16 +0300

    fix(sprint_2): test fixes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import autoprefixer from "autoprefixer";
 import nested from "postcss-nested";
 import customProperties from "postcss-custom-properties";
 import eslint from "vite-plugin-eslint";
-import stylelint from "vite-plugin-stylelint";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -14,7 +13,7 @@ export default defineConfig({
     //@ts-expect-error
     handlebars(),
     eslint(),
-    stylelint(),
+    // stylelint(),
   ],
   css: {
     postcss: {
